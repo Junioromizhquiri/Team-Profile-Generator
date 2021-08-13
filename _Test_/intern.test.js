c8e6396ecf673164bb4a1f4256ec67e2372926a7
Junior Mizhquiri <Junioromizhquiri@gmail.com>
2021-08-13 17:21:35 -0400

    fixed what was missing

---
+++ _Test_/intern.test.js
@@ -0,0 +1,27 @@
+const Intern = require("./lib/Intern");
+
+describe("Intern", () => {
+  describe("getSchool", () => {
+    it("should return school of Intern", () => {
+        const testIntern = new Intern("John Smith", "email@email.com", "123", "school");
+        const school = "school";
+
+        const result = testIntern.getSchool();
+
+        
+        expect(result).toEqual(school);
+    });
+  });
+
+  describe("getRole", () => {
+    it("should return role of Intern", () => {
+        const testIntern = new Intern("John Smith", "email@email.com", "123", "username");
+        const role = "Intern";
+        
+        const result = testIntern.getRole();
+
+        
+        expect(result).toEqual(role);
+    });
+  });
+});
